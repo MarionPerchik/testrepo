ffb6bd58a1872ec0355a42b74714866361008345
Marion <slehbeh@yandex.ru>
2023-09-28 18:42:34 +0300

    lab6 done

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -7,44 +7,12 @@ using System.Threading.Tasks;
 namespace TestApp
 {
     class Program
-    {
-        public static void ClearLine()
-        {
-            Console.SetCursorPosition(0, Console.CursorTop - 1);
-            Console.Write(new string(' ', Console.WindowWidth));
-            Console.SetCursorPosition(0, Console.CursorTop - 1);
-        }
+    { 
 
         static void Main(string[] args)
         {
-            /*Point p1 = new Point(2, 2, '*');
-            p1.Draw();
-            Point p2 = new Point(2, 2, '*');
-            p2.Draw();
-            if (p1.IsHit(p2))
-            {
-                Console.SetCursorPosition(0, 6);
-                Console.WriteLine("Точки пересекаются");
-            }
-            else
-            {
-                Console.SetCursorPosition(0, 6);
-                Console.WriteLine("Точки не пересекаются");
-            }*/
-            HorizontalLine h1 = new HorizontalLine(3, 10, 3, '*');
-            h1.Draw();
-            VerticalLine v1 = new VerticalLine(0,7,12,'*');
-            v1.Draw();
-            if (h1.IsHit(v1))
-            {
-                Console.SetCursorPosition(0, 15);
-                Console.WriteLine("Линии пересекаются");
-            }
-            else
-            {
-                Console.SetCursorPosition(0, 15);
-                Console.WriteLine("Линии не пересекаются");
-            }
+            Walls wall = new Walls(Console.WindowWidth, Console.WindowHeight);
+            wall.Draw();
             Console.ReadKey();
         }
     }
